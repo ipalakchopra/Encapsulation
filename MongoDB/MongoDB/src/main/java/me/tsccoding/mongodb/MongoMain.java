1614c593226241f44de44f082c1fe31349556a17
Zain1441 <zain.azmi@somaiya.edu>
2023-12-03 20:48:59 +0530

    Login & Register

--- MongoDB/MongoDB/src/main/java/me/tsccoding/mongodb/MongoMain.java
+++ MongoDB/MongoDB/src/main/java/me/tsccoding/mongodb/MongoMain.java
@@ -1,5 +1,5 @@
 package me.tsccoding.mongodb;
-
+import java.util.Scanner;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
@@ -9,6 +9,7 @@ import org.bson.conversions.Bson;
 
 public class MongoMain {
     public static void main(String args[]) {
+
         String uri = "mongodb+srv://admin:admin@testcluster.yvs3ank.mongodb.net/";
         MongoClientURI clientURI = new MongoClientURI(uri);
         MongoClient mongoClient = new MongoClient(clientURI);
@@ -18,23 +19,60 @@ public class MongoMain {
 
         System.out.println("Database Connected");
 
-        String username="zain";
-        String incoming_id="abcd";
-        String outgoing_id="wxyz";
+        Scanner inputch = new Scanner(System.in);
+        System.out.println("Login (1) or Register (2)");
+        int choice = inputch.nextInt();
+
+    if(choice == 1)
+    {
+        //Login
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("Enter Username : ");
+        String username = input1.next();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Enter Password : ");
+        String password = input2.next();
+        Document founduser = (Document) collection.find(new Document("username", username)).first();
+        Document foundpass = (Document) collection.find(new Document("password", password)).first();
+
+       if(founduser == null){
+            System.out.println("User not found");
+       }
+       else if(founduser != null && foundpass == null){
+        System.out.println("inncorrect pass");
+       }
+       else{
+            System.out.println("Login seccess!");
+       }
+    }
+
+    else{
+        //Register
+        Scanner input3 = new Scanner(System.in);
+        System.out.println("Enter Username : ");
+        String username = input3.next();
+
+        Scanner input4 = new Scanner(System.in);
+        System.out.println("Enter Password : ");
+        String password = input4.next();
+
+        Document founduser = (Document) collection.find(new Document("username", username)).first();
+
+        if(founduser != null){
+            System.out.println("Username already Exists!");
+       }
+       else{
         Document document = new Document ("username", username) ;
-        document.append ("incomingID", incoming_id);
-        document.append ("outgoingID", outgoing_id);
-
+        document.append ("password", password);
         collection.insertOne(document);
-        // Document found = (Document) collection.find(new Document("username", "zain")).first();
-
-       //f(found != null){
-            //System.out.println("Found User");
-
+        System.out.println("User successfully created!");
+       }
+    }
+    }
             //Bson updatedvalue = new Document("Age", 26);
             //Bson updateoperation = new Document("$set", updatedvalue);
             //collection.updateOne(found,updateoperation);
             //System.out.println("User Updated");
-        //}
-    }
 }
+
