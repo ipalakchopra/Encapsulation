fbe0df9e31ba18c9f36175cf4e8cc264888bb6cd
ipalakchopra <72378463+ipalakchopra@users.noreply.github.com>
2023-12-08 00:46:17 +0530

    AES implemented

--- src/Decapsulation.java
+++ src/Decapsulation.java
@@ -4,15 +4,15 @@ import java.util.*;
 import java.io.*;
 import java.nio.*;
 import java.nio.file.*;
-
+import javax.crypto.NoSuchPaddingException;
 public class Decapsulation {
     
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException,NoSuchPaddingException {
         
         Container c_decap = new Container();
 
         try{
-            RandomAccessFile file_in = new RandomAccessFile("enca_file.sec","r");
+            RandomAccessFile file_in = new RandomAccessFile("encap_filenew.sec","r");
             
             String u_id = "ifkhtCNnIc";
 
@@ -34,10 +34,18 @@ public class Decapsulation {
                 //System.out.println("hello");
                 
                 String fl_ext = file_in.readLine();
+                String strkey = file_in.readLine();
+                //String iv = file_in.readLine();
+                System.out.println(strkey);
+                //System.out.println(iv);
+                c_decap.secretkey = strkey.getBytes();
                 c_decap.file_extension = fl_ext.getBytes();
-                System.out.println(c_decap.file_extension.length);
+                System.out.println(new String(c_decap.file_extension));
+                //System.out.println(new String(c_decap.secretkey));
                 
-                int file_sz = ((int)file_in.length()/1024*1024) - 20 - c_decap.file_extension.length;
+                
+
+                /*int file_sz = ((int)file_in.length()/1024*1024) - 20 - c_decap.file_extension.length - c_decap.iv.length - c_decap.secretkey.length;
 
                 System.out.println(file_sz);
 
@@ -45,14 +53,34 @@ public class Decapsulation {
                 
                 file_in.read(c_decap.byte_file);
                 
-                String filestr = new String(c_decap.byte_file);
+                String encfile = new String(c_decap.byte_file);*/
+                String encfile = file_in.readLine();
+                c_decap.iv = new byte[16];
+
+                file_in.read(c_decap.iv);
+                System.out.println(encfile);
+                String decfile;
+                try {
+                    System.out.println("hello");
+                    decfile = File_AES.DecDriver(strkey, encfile, c_decap.iv);
+                    //System.out.println(decfile);
+                    byte[] dec = Base64.getDecoder().decode(decfile);
+                    Path decap_path = Paths.get(String.format("decap_filenew%s",fl_ext));
+                    Files.write(decap_path, dec);
+                } catch (Exception e) {
+                }
+                
+
+                //System.out.println(striv);
+                //System.out.println(strkey);
+                
+                /*String filestr = new String(c_decap.byte_file);
                 //file_in.read(c_decap.byte_file);
 
                 System.out.println(fl_ext);
-                System.out.println(filestr);
+                System.out.println(filestr);*/
 
-                Path decap_path = Paths.get(String.format("decap_file%s",fl_ext));
-                Files.write(decap_path, c_decap.byte_file);
+               
 
             }
 
--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -4,11 +4,11 @@ import src.Container;
 
 import java.util.*;
 
+import javax.crypto.NoSuchPaddingException;
 import javax.naming.ldap.Rdn;
 
 import java.io.*;
 import java.nio.file.*;
-
 
 public class Encapsulation {
 
@@ -22,7 +22,7 @@ public class Encapsulation {
             obj_out.close();
             file_out.close();*/
 
-            RandomAccessFile file_out = new RandomAccessFile("enca_file.sec","rw");
+            RandomAccessFile file_out = new RandomAccessFile("encap_filenew.sec","rw");
 
             
             /*file_out.write(c.file_extension, 12, c.file_extension.length);
@@ -33,8 +33,14 @@ public class Encapsulation {
             file_out.write(c.receiver_id);
             file_out.write(c.file_extension);
             file_out.write("\n".getBytes());
+            file_out.write(c.secretkey);
+            file_out.write("\n".getBytes());
             file_out.write(c.byte_file);
-             
+            file_out.write("\n".getBytes());
+            file_out.write(c.iv);
+            
+            
+            
             file_out.close();
             
 
@@ -46,9 +52,11 @@ public class Encapsulation {
         }
     }
 
-    public static void main(String[] args) throws FileNotFoundException,IOException{
+    public static void main(String[] args) throws FileNotFoundException,IOException,NoSuchPaddingException{
 
         Container c = new Container();
+
+        File_AES AESUtil = new File_AES();
 
         Path file_path = Paths.get("E:\\College\\Sem_7\\LY_Project\\Encapsulation\\test_pdf.pdf");
 
@@ -56,11 +64,28 @@ public class Encapsulation {
         
         String s_id = "DL0KG6WZwu";
         String r_id = "ifkhtCNnIc";
-
+        
+        
         c.file_extension = file_ext.substring(file_ext.indexOf(".")).getBytes();
         
-        c.byte_file = Files.readAllBytes(file_path);
-    
+        byte[] byte_file = Files.readAllBytes(file_path);
+        
+        try {
+            String string_file = Base64.getEncoder().encodeToString(byte_file);
+
+            String keyString = File_AES.Key_Gen(r_id, s_id);
+            System.out.println(keyString);
+            
+            c.secretkey = keyString.getBytes();
+
+            byte[] iv = File_AES.generateIv();
+            c.iv = iv;
+            string_file = File_AES.EncDriver(keyString, string_file, iv);
+            System.out.println(string_file.substring(0,4));
+            c.byte_file = string_file.getBytes();
+        } catch (Exception e) {
+            
+        }
         c.sender_id = s_id.getBytes();
         
         c.receiver_id = r_id.getBytes();
